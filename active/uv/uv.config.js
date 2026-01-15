c3f6bf533ec0fd0810dfb77ceedefa468abbf916
uheamd84lalsdurhagjnxaa34 <baktashanwari234@gmail.com>
2026-01-15 14:44:50 -0800

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://qmsschool.truckstore.ch/bare/',
+    bare:'https://bare-server-mkcl.onrender.com/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
