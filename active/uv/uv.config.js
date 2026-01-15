5d7a72eee84e4716c155025c927912d32f45963b
uheamd84lalsdurhagjnxaa34 <baktashanwari234@gmail.com>
2026-01-15 15:00:57 -0800

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/active/go/',
-    bare:'https://wisp.mercurywork.shop/',
+    bare:'https://bare.nixnet.services/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
